fc41f6162e52be5d0bae0db1687e3957d7dcb773
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-05-09 09:30:06 -0600

    added two tests to check that it exists and that we can change english to morse code

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,16 @@
-# YOUR TESTS GO IN HERE!
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/translator'
+
+class TranslatorTest < Minitest::Test
+  def test_that_it_exists
+    translator = Translator.new
+    assert_instance_of Translator, translator
+  end
+
+  def test_it_can_change_from_english_to_morse_code
+    translator = Translator.new
+    expected = "......-...-..--- .-----.-..-..-.."
+   assert_equal expected, translator.eng_to_morse("hello world")
+  end
+end
